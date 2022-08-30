060963b4aa6b89e8199d49fb2aefdb27f05083da
ATSUHIRO MATSUZAKI <55480334+matsuzaki0817@users.noreply.github.com>
2022-08-30 17:38:57 +0900

    スタートボタンの表示/非表示も追加

--- index.js
+++ index.js
@@ -2,13 +2,22 @@ const startBtn = document.querySelector("#startBtn");
 const restartBtn = document.querySelector("#restartBtn");
 const container = document.querySelector("#container");
 
+// スタートボタンを押すとゲームが表示され、スタートボタンが非表示になる
 startBtn.addEventListener("click", () => {
-  if(getComputedStyle(container).display == 'none'){
+  if (getComputedStyle(container).display == 'none') {
     container.style.display = "block";
   }
+  if (getComputedStyle(startBtn).display == 'inline-block') {
+    startBtn.style.display = "none";
+  }
 })
+
+// リスタートボタンを押すとゲームが非表示になり、スタートボタンが表示される
 restartBtn.addEventListener("click", () => {
-  if(getComputedStyle(container).display == 'block'){
+  if (getComputedStyle(container).display == 'block') {
     container.style.display = "none";
+  }
+  if (getComputedStyle(startBtn).display == 'none') {
+    startBtn.style.display = "inline-block";
   }
 })
