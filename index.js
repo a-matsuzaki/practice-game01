ba4635cdd626174bb3ae34869773e0853a571e5a
ATSUHIRO MATSUZAKI <55480334+matsuzaki0817@users.noreply.github.com>
2022-09-01 22:05:31 +0900

    ゲームの初期化を関数化（途中まで）

--- index.js
+++ index.js
@@ -12,6 +12,17 @@ let currentPlayer = "O";
 // ゲームの開始のタイミングでtrueにする
 let running = false;
 
+// ゲーム初期化
+initializeGame();
+
+// ゲームを初期化する関数
+function initializeGame() {
+  // 開始時のプレイヤー名を表示
+  result.textContent = `${currentPlayer}のターン`;
+  // ゲーム実行中
+  running = true;
+}
+
 // スタートボタンを押したときの挙動
 function startGame() {
   // ゲーム画面を表示
@@ -23,6 +34,7 @@ function startGame() {
     startBtn.style.display = "none";
   }
 }
+
 // リスタートボタンを押したときの挙動
 function restartGame() {
   // ゲーム画面を非表示
