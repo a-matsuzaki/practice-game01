caec1dd666cc3881e448c9de041887f24d126a36
ATSUHIRO MATSUZAKI <55480334+matsuzaki0817@users.noreply.github.com>
2022-09-06 22:11:18 +0900

    スタートボタンとリスタートボタンの実装

--- index.js
+++ index.js
@@ -131,29 +131,24 @@ function restartGame() {
   });
   // ゲームは実行中に
   running = true;
+  // ゲーム画面を非表示
+  if (getComputedStyle(container).display == 'block') {
+    container.style.display = "none";
+  }
+  // スタートボタンを表示
+  if (getComputedStyle(startBtn).display == 'none') {
+    startBtn.style.display = "inline-block";
+  }
 }
 // スタートボタンを押したときの挙動
-// function startGame() {
-//   // ゲーム画面を表示
-//   if (getComputedStyle(container).display == 'none') {
-//     container.style.display = "block";
-//   }
-//   // スタートボタンを非表示
-//   if (getComputedStyle(startBtn).display == 'inline-block') {
-//     startBtn.style.display = "none";
-//   }
-// }
-
-// リスタートボタンを押したときの挙動
-// function restartGame() {
-//   // ゲーム画面を非表示
-//   if (getComputedStyle(container).display == 'block') {
-//     container.style.display = "none";
-//   }
-//   // スタートボタンを表示
-//   if (getComputedStyle(startBtn).display == 'none') {
-//     startBtn.style.display = "inline-block";
-//   }
-// }
-
+startBtn.addEventListener("click", function () {
+  // ゲーム画面を表示
+  if (getComputedStyle(container).display == 'none') {
+    container.style.display = "block";
+  }
+  // スタートボタンを非表示
+  if (getComputedStyle(startBtn).display == 'inline-block') {
+    startBtn.style.display = "none";
+  }
+});
 
